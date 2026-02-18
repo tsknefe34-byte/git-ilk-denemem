2f912785a3e45d17a325b0ef907303c1728b782d
efe taskin <tsknefe34@gmail.com>
2026-02-18 23:03:23 +0300

    asal sayi olayini yaptim fakat 2ve 3 hesaplayamiyor

--- derleme.cpp
+++ derleme.cpp
@@ -3,26 +3,28 @@
 using namespace std;
 int main() {
 
-int number,i, result;
+int number,i, result,control ;
 
-cout <<"bir sayi girin"<<endl;
+cout <<"bir sayi girin\t";
 cin >> number ;
 
 if (number<=1){
     cout<<"1den buyuk sayi girin lutfen"<<endl;
-    return 0;
+    
 }
 
 result=sqrt(number);
 for (i=2; i<=result; i++){
    
-    if(number%result==0){
-        cout<<"bu bir asal sayi"<<endl;
+    if(number%i==0){
+        control=2;
     }
-    else 
-        cout<<"bu bir asal sayi degil"<<endl;
 }
 
+if (control==2)
+    cout<<"bu bir asal sayi degil"<<endl;
+else
+    cout<<"bu bir asal sayi"<<endl;
 
     return 0;
 }
