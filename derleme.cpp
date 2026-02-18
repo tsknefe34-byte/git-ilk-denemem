6e92b813774b548a1ea4ca724c5067e2d5dfaeda
efe taskin <tsknefe34@gmail.com>
2026-02-18 22:43:48 +0300

    asal sayi

--- derleme.cpp
+++ derleme.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main() {
-    cout << "Hello VSCode C++!" <<endl;
-    cout<< "bu da commit icin deneme";
-    cout<< "bu da mac icin commit denemesi";
-    cout << "bu da mac icin pull denemesi";
+
+int number,i, result;
+
+cout <<"bir sayi girin"<<endl;
+cin >> number ;
+
+if (number<=1){
+    cout<<"1den buyuk sayi girin lutfen"<<endl;
     return 0;
 }
+
+result=sqrt(number);
+for (i=2; i<=result; i++){
+   
+    if(number%result==0){
+        cout<<"bu bir asal sayi"<<endl;
+    }
+    else 
+        cout<<"bu bir asal sayi degil"<<endl;
+}
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
