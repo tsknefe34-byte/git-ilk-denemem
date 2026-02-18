0e71743453a0cd4082a95c4f882a361eeeb4ab7e
efe taskin <tsknefe34@gmail.com>
2026-02-18 16:14:28 +0300

    mac pull denemesi

--- derleme.cpp
+++ derleme.cpp
@@ -4,5 +4,6 @@ int main() {
     cout << "Hello VSCode C++!" <<endl;
     cout<< "bu da commit icin deneme";
     cout<< "bu da mac icin commit denemesi";
+    cout << "bu da mac icin pull denemesi";
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello VSCode C++!" <<endl
+    cout << "Hello VSCode C++!" <<endl;
+    cout<< "bu da commit icin deneme";
+    cout<< "bu da mac icin commit denemesi";
+    cout << "bu da mac icin pull denemesi";
     return 0;
 }
