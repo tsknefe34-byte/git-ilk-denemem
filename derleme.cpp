949613b5ad4df5797e55825140a22fa012580a41
efe taskin <tsknefe34@gmail.com>
2026-02-19 12:31:39 +0300

    sorunsuz calisiyor suan

--- derleme.cpp
+++ derleme.cpp
@@ -5,19 +5,22 @@ int main() {
 
 int number,i, result,control ;
 
-cout <<"bir sayi girin\t";
+cout << "bir sayi girin\t";
 cin >> number ;
 
 if (number<=1){
     cout<<"1den buyuk sayi girin lutfen"<<endl;
-    
+    return 0;
 }
 
 result=sqrt(number);
 for (i=2; i<=result; i++){
-   
+
     if(number%i==0){
         control=2;
+    }
+    else{
+        control=1;
     }
 }
 
